151ac6feb21f1758c1a808ce1d83e14e51bd8b79
pranavkrishna-sn <pranav05krish@gmail.com>
2026-01-13 15:19:15 +0530

    Delete backend directory

--- backend/models/category.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from pydantic import BaseModel, Field
-
-class Category(BaseModel):
-    id: int | None = None
-    name: str = Field(..., min_length=3, max_length=50)
-    description: str | None = None
-    created_at: datetime = Field(default_factory=datetime.utcnow)
-    updated_at: datetime = Field(default_factory=datetime.utcnow)
